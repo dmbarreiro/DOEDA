58c503e6e63f8d7aac8c7fe1275884e45ad6076c
David Martinez Barreiro <5133990+dmbarreiro@users.noreply.github.com>
2022-05-19 00:18:22 +0200

    local experiment validation script and documentation
    script validation used in github actions pipeline can
    now be executed locally to test your experiment files
    locally. Added brief documentation for the command too

--- utils/validation/scripts/validation.py
+++ utils/validation/scripts/validation.py
@@ -1,39 +1,51 @@
-import yamale
-from git import Repo
 import re
 
+import yamale
+from git import Repo
 from yaml import YAMLError
+import click
 
-try:
-    experiment_schema = yamale.make_schema("utils/validation/schemas/experiment.yaml")
-    repo = Repo(".")
-    files = repo.git.execute(["git", "diff", "--name-only", "main"]).split("\n")
-    experiments_re = re.compile(r"^experiments\/.+")
-    experiments_files = list(
-        filter(lambda filename: bool(experiments_re.match(filename)), files)
-    )
-    experiments_files = ["experiments/test.yaml"]
-    for experiment in experiments_files:
-        experiment_data = yamale.make_data(experiment)
-        yamale.validate(experiment_schema, experiment_data)
-        n_runs = experiment_data[0][0].get("run_size")
-        dataset = experiment_data[0][0].get("dataset")
-        response = experiment_data[0][0].get("response", None)
-        # Validate dataset size
-        for data in dataset:
-            coded_data = data.get("coded")
-            uncoded_data = data.get("uncoded", None)
-            if len(coded_data) != n_runs:
-                raise YAMLError("Mismatch in dataset coded data and number of runs")
-            if uncoded_data and len(uncoded_data) != n_runs:
-                raise YAMLError("Mismatch in dataset uncoded data and number of runs")
-        # Validate response size
-        if response:
-            for data in response:
-                value = data.get("value")
-                if len(value) != n_runs:
-                    raise YAMLError("Mismatch in response values and number of runs")
-    print("Validation success! 👍")
-except Exception as e:
-    print("Validation failed!\n%s" % str(e))
-    exit(1)
+
+@click.command()
+@click.option('--file', help='Path to the file to validate.')
+def validate(file):
+    try:
+        experiment_schema = yamale.make_schema("utils/validation/schemas/experiment.yaml")
+        if file:
+            experiments_files = [file]
+        else:
+            
+            repo = Repo(".")
+            files = repo.git.execute(["git", "diff", "--name-only", "main"]).split("\n")
+            experiments_re = re.compile(r"^experiments\/.+")
+            experiments_files = list(
+                filter(lambda filename: bool(experiments_re.match(filename)), files)
+            )
+        for experiment in experiments_files:
+            experiment_data = yamale.make_data(experiment)
+            yamale.validate(experiment_schema, experiment_data)
+            n_runs = experiment_data[0][0].get("run_size")
+            dataset = experiment_data[0][0].get("dataset")
+            response = experiment_data[0][0].get("response", None)
+            # Validate dataset size
+            for data in dataset:
+                coded_data = data.get("coded")
+                uncoded_data = data.get("uncoded", None)
+                if len(coded_data) != n_runs:
+                    raise YAMLError("Mismatch in dataset coded data and number of runs")
+                if uncoded_data and len(uncoded_data) != n_runs:
+                    raise YAMLError("Mismatch in dataset uncoded data and number of runs")
+            # Validate response size
+            if response:
+                for data in response:
+                    value = data.get("value")
+                    if len(value) != n_runs:
+                        raise YAMLError("Mismatch in response values and number of runs")
+        print("Validation success! 👍")
+    except Exception as e:
+        print("Validation failed!\n%s" % str(e))
+        exit(1)
+
+
+if __name__ == "__main__":
+    validate()
