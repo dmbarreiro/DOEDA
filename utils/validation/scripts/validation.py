f6e843185a7bbbd5885bea73964787681225cea7
David Martinez Barreiro <5133990+dmbarreiro@users.noreply.github.com>
2022-06-09 23:59:21 +0000

    add upload design bundle to s3 manual action
    the action generates a bundle with all the experiments present
    in the repository at that moment and uploads it to our AWS
    S3 bucket

--- utils/validation/scripts/validation.py
+++ utils/validation/scripts/validation.py
@@ -5,21 +5,23 @@ from git import Repo
 from yaml import YAMLError
 import click
 
-keywords_file = 'utils/validation/keywords.yaml'
+keywords_file = "utils/validation/keywords.yaml"
 
 
 @click.command()
-@click.option('--file', help='Path to the file to validate.')
+@click.option("--file", help="Path to the file to validate.")
 def validate(file):
     try:
         experiment_schema = yamale.make_schema(
-            "utils/validation/schemas/experiment.yaml")
+            "utils/validation/schemas/experiment.yaml"
+        )
         if file:
             experiments_files = [file]
         else:
             repo = Repo(".")
             files = repo.git.execute(
-                ["git", "diff", "--name-only", "origin/main"]).split("\n")
+                ["git", "diff", "--name-only", "origin/main"]
+            ).split("\n")
             experiments_re = re.compile(r"^experiments\/.+")
             experiments_files = list(
                 filter(lambda filename: bool(experiments_re.match(filename)), files)
@@ -28,10 +30,15 @@ def validate(file):
             experiment_data = yamale.make_data(experiment)
             yamale.validate(experiment_schema, experiment_data)
             # validate keywords
-            valid_keywords = yamale.make_data(keywords_file)[0][0]['keywords']
+            valid_keywords = yamale.make_data(keywords_file)[0][0]["keywords"]
             keywords = experiment_data[0][0].get("keywords")
-            all_kw_valid = all(map(lambda kw: kw.lower() in list(
-                map(lambda valid_kw: valid_kw.lower(), valid_keywords)), keywords))
+            all_kw_valid = all(
+                map(
+                    lambda kw: kw.lower()
+                    in list(map(lambda valid_kw: valid_kw.lower(), valid_keywords)),
+                    keywords,
+                )
+            )
             if all_kw_valid == False:
                 raise YAMLError("Experiment contains invalid keywords")
             # validate dataset and response
@@ -46,15 +53,17 @@ def validate(file):
                     raise YAMLError("Mismatch in dataset coded data and number of runs")
                 if uncoded_data and len(uncoded_data) != n_runs:
                     raise YAMLError(
-                        "Mismatch in dataset uncoded data and number of runs")
+                        "Mismatch in dataset uncoded data and number of runs"
+                    )
             # Validate response size
             if response:
                 for data in response:
                     value = data.get("value")
                     if len(value) != n_runs:
                         raise YAMLError(
-                            "Mismatch in response values and number of runs")
-        print("Validation success! 👍")
+                            "Mismatch in response values and number of runs"
+                        )
+        print("✅ Validation success")
     except Exception as e:
         print("Validation failed!\n%s" % str(e))
         exit(1)
