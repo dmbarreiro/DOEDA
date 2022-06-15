eff64259cc13ca79c56fd1ecfc5a26f94e5be38b
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2022-06-15 10:48:28 +0200

    Added CLI interface

--- utils/data_processing/recode_csv.py
+++ utils/data_processing/recode_csv.py
@@ -1,52 +1,63 @@
 """
 Recode the levels of the factors in a csv file.
-Work In Progress: 
-    File path and levels encoding are hardcoded, they should
-be input parameters (inline in case of filepath and through a file
-in case of levels)
-Levels file format (for instance):
-    [
-        {'index': 1,
-         'map': {-1: 0, 0: 200, 1: 400}},
-        {'index': 2,
-         'map': {-1: 0, 0: 500, 1: 1000}},
-        {'index': 3,
-         'map': {-1: 0, 0: 500, 1: 1000}},
-        {'index': 4,
-         'map': {-1: 0, 0: 1000, 1: 2000}},
-        {'index': 5,
-         'map': {-1: 0, 0: 400, 1: 800}},
-    ]
 """
-from typing import List, Dict
+import json
+import click
 import pandas as pd
 
 
-def main(path: str, levels_eq: List[Dict], sep=";", decimal=","):
+@click.command()
+@click.option('-f',
+              '--filepath',
+              type=str,
+              required=True,
+              help='Path of the file to recode')
+@click.option('-l',
+              '--levels-filepath',
+              type=str,
+              required=True,
+              help="Path of the JSON file containing the new levels.")
+def main(filepath, levels_filepath, sep=";", decimal=","):
+    """
+    Recode the levels of one or several columns in a CSV file using a levels-recoding file.
+
+    The levels-recoding file is a JSON file with an array containing one element per
+    column to recode.
+    Each element contains two fields:
+    - "index": the index (0-based) of the column to recode in the csv file.
+    - "map": a map of the value to recode. This map contains itself one field per
+    level to recode in the column, written in pairs {"old": new}
+
+    For example, if we want to recode the third column of our csv file in the
+    following way: -1 to 0, 0 to 100 and 1 to 200, the levels-recoding JSON file
+    will be:
+
+    [{"index": 2,"map": {"-1": 0, "0": 100, "1": 200}}]
+    """
+    try:
+        with open(levels_filepath) as f:
+            levels_eq = json.load(f)
+    except Exception as e:
+        print('Could not read the JSON file containing the new levels. %s' % e)
+        exit(1)
     # Load the csv file
-    df = pd.read_csv(filepath, sep=sep, decimal=decimal)
+    try:
+        df = pd.read_csv(filepath, sep=sep, decimal=decimal)
+    except Exception as e:
+        print('Could not open the CSV file. %s' % e)
+        exit(1)
     # Recode the variables
     columns = df.columns
     for level in levels_eq:
         col = columns[level.get("index")]
-        df[col] = df[col].map(level.get("map"))
+        col_map = {int(k): v for k, v in level.get("map").items()}
+        df[col] = df[col].map(col_map)
     # Save the new df to csv file
-    new_path = path.replace(".csv", "_recoded.csv")
+    new_path = filepath.replace(".csv", "_recoded.csv")
     df.to_csv(new_path, sep=sep, decimal=decimal, index=False)
+    print('CSV sucessfully recoded.')
+    exit(0)
 
 
 if __name__ == "__main__":
-    # TODO adapt these hardcoded path and encodings
-    filepath = "csv/polyethylene_film.csv"
-    levels_recoding = [
-        {"index": 1, "map": {-1: 0, 0: 200, 1: 400}},
-        {"index": 2, "map": {-1: 0, 0: 500, 1: 1000}},
-        {"index": 3, "map": {-1: 0, 0: 500, 1: 1000}},
-        {"index": 4, "map": {-1: 0, 0: 1000, 1: 2000}},
-        {"index": 5, "map": {-1: 0, 0: 400, 1: 800}},
-    ]
-    try:
-        main(filepath, levels_recoding)
-        print("CSV sucessfully recoded.")
-    except Exception as e:
-        print(f"Could not recode csv file. {e}")
+    main()
